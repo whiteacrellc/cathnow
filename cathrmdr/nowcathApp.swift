b23e26b7d95dfd4c1bae59d1f26d9c2c95b5c275
White Acre Software <tom@tomw.net>
2025-11-08 13:10:20 -0600

    fixed warning
    fixed build warnings and version mismatch

--- cathrmdr/nowcathApp.swift
+++ cathrmdr/nowcathApp.swift
@@ -60,7 +60,7 @@ class NotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         // Show notification even when app is in foreground
-        completionHandler([.alert, .sound, .badge])
+        completionHandler([.banner, .list, .sound, .badge])
     }
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
